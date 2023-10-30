7512568c849dac590b5d7c6bb3299322acecf9fc
Ady G Code <ady.g.coder@gmail.com>
2023-10-31 07:09:24 +0800

    init: Base Portfolio Site

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './phase-*/*.html',
+    './pages/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
